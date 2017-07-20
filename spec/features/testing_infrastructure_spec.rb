a2960b6f58db25287b495e6edffe0b150e8ea9de
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-20 12:50:20 +0100

    Completed POST/redirect/GET

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
-    visit('/')
+    visit('/testing')
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
